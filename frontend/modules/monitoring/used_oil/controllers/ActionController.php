8d7e5dd30188fbb9ecfa5209d1f5f748383d076c
agik-setiawan <setiawanagik@gmail.com>
2017-06-05 13:53:23 +0700

    bower auto

--- frontend/modules/monitoring/used_oil/controllers/ActionController.php
+++ frontend/modules/monitoring/used_oil/controllers/ActionController.php
@@ -76,18 +76,21 @@ return $datatables->getResponse();
 
 //data fmc apa saja
 elseif (substr($dataId,0,3)=="fmc" && strlen($dataId)>3) {
-  $query->select(['tbl_transaction.grouploc','tbl_transaction.branch','tbl_transaction.name as customer_name',
-  'tbl_transaction.lab_no','tbl_transaction.sampl_dt1 as sample_date','tbl_transaction.recv_dt1 as receive_date',
-'tbl_transaction.rpt_dt1 as report_date','tbl_transaction.unit_no as unit_number','tbl_transaction.COMPONENT as component_name','tbl_transaction.model','tbl_transaction.oil_change','tbl_transaction.filter_code','(select "") as cmp',
-'(select "") as mp','(select case tbl_transaction.eval_code when "N" then "Normal"
-when "B" then "Attention"
-when "C" then "Urgent"
-when " " then "Normal"
-when null then "Normal" end) as eval_code','(select "")as blank'])
-  ->from('tbl_transaction')
-  ->where(['tbl_transaction.grouploc'=>$dataId])
-  ->limit(10000);
-  return $query->all();
+  $sql="tbl_transaction.grouploc,tbl_transaction.branch,tbl_transaction.name,tbl_transaction.Lab_No,
+tbl_transaction.SAMPL_DT1,tbl_transaction.RECV_DT1,tbl_transaction.RPT_DT1,
+tbl_transaction.UNIT_NO,tbl_transaction.COMPONENT,tbl_transaction.MODEL,
+tbl_transaction.oil_change,tbl_transaction.EVAL_CODE";
+$where="tbl_transaction.grouploc='$dataId' and tbl_transaction.rpt_dt1 > (DATE_SUB(CURDATE(), INTERVAL 3 YEAR))";
+
+ $datatables = (new DataTables\Builder())
+    ->withQueryBuilder(
+        $conn->createQueryBuilder()
+            ->select($sql)
+            ->from('tbl_transaction')
+            ->where($where)
+    )
+    ->withRequestParams($_REQUEST);
+return $datatables->getResponse();
 }
 
 //branch,customer,grouploc
@@ -126,17 +129,21 @@ $conn=Configdb::connections();
 
    //admin and ho role
 if ($dataId=="admin" || $dataId=="ho") {
-  $query->select(['tbl_transaction.grouploc','tbl_transaction.branch','tbl_transaction.name as customer_name',
-  'tbl_transaction.lab_no','tbl_transaction.sampl_dt1 as sample_date','tbl_transaction.recv_dt1 as receive_date',
-'tbl_transaction.rpt_dt1 as report_date','tbl_transaction.unit_no as unit_number','tbl_transaction.COMPONENT as component_name','tbl_transaction.model','tbl_transaction.oil_change','tbl_transaction.filter_code','(select "") as cmp',
-'(select "") as mp','(select case tbl_transaction.eval_code when "N" then "Normal"
-when "B" then "Attention"
-when "C" then "Urgent"
-when " " then "Normal"
-when null then "Normal" end) as eval_code','(select "")as blank'])
-  ->from('tbl_transaction')
-  ->where(['between','tbl_transaction.recv_dt1',$date1,$date2]);
-return $query->all();
+  $sql="tbl_transaction.grouploc,tbl_transaction.branch,tbl_transaction.name,tbl_transaction.Lab_No,
+tbl_transaction.SAMPL_DT1,tbl_transaction.RECV_DT1,tbl_transaction.RPT_DT1,
+tbl_transaction.UNIT_NO,tbl_transaction.COMPONENT,tbl_transaction.MODEL,
+tbl_transaction.oil_change,tbl_transaction.EVAL_CODE";
+$where="tbl_transaction.rpt_dt1 > (DATE_SUB(CURDATE(), INTERVAL 3 YEAR)) and (tbl_transaction.RECV_DT1 between '$date1' and '$date2')";
+
+$datatables = (new DataTables\Builder())
+    ->withQueryBuilder(
+        $conn->createQueryBuilder()
+            ->select($sql)
+            ->from('tbl_transaction')
+            ->where($where)
+    )
+    ->withRequestParams($_REQUEST);
+return $datatables->getResponse();
 }
 
 //data fmcpusat
@@ -212,17 +219,21 @@ $conn=Configdb::connections();
 
         //admin and ho role
 if ($dataId=="admin" || $dataId=="ho") {
-  $query->select(['tbl_transaction.grouploc','tbl_transaction.branch','tbl_transaction.name as customer_name',
-  'tbl_transaction.lab_no','tbl_transaction.sampl_dt1 as sample_date','tbl_transaction.recv_dt1 as receive_date',
-'tbl_transaction.rpt_dt1 as report_date','tbl_transaction.unit_no as unit_number','tbl_transaction.COMPONENT as component_name','tbl_transaction.model','tbl_transaction.oil_change','tbl_transaction.filter_code','(select "") as cmp',
-'(select "") as mp','(select case tbl_transaction.eval_code when "N" then "Normal"
-when "B" then "Attention"
-when "C" then "Urgent"
-when " " then "Normal"
-when null then "Normal" end) as eval_code','(select "")as blank'])
-  ->from('tbl_transaction')
-  ->where(['between','tbl_transaction.sampl_dt1',$date1,$date2]);
-return $query->all();
+  $sql="tbl_transaction.grouploc,tbl_transaction.branch,tbl_transaction.name,tbl_transaction.Lab_No,
+tbl_transaction.SAMPL_DT1,tbl_transaction.RECV_DT1,tbl_transaction.RPT_DT1,
+tbl_transaction.UNIT_NO,tbl_transaction.COMPONENT,tbl_transaction.MODEL,
+tbl_transaction.oil_change,tbl_transaction.EVAL_CODE";
+$where="tbl_transaction.rpt_dt1 > (DATE_SUB(CURDATE(), INTERVAL 3 YEAR)) and (tbl_transaction.SAMPL_DT1 between '$date1' and '$date2')";
+
+$datatables = (new DataTables\Builder())
+    ->withQueryBuilder(
+        $conn->createQueryBuilder()
+            ->select($sql)
+            ->from('tbl_transaction')
+            ->where($where)
+    )
+    ->withRequestParams($_REQUEST);
+return $datatables->getResponse();
 }
 
 //data fmcpusat
@@ -297,17 +308,21 @@ $conn=Configdb::connections();
 
             //admin and ho role
 if ($dataId=="admin" || $dataId=="ho") {
-  $query->select(['tbl_transaction.grouploc','tbl_transaction.branch','tbl_transaction.name as customer_name',
-  'tbl_transaction.lab_no','tbl_transaction.sampl_dt1 as sample_date','tbl_transaction.recv_dt1 as receive_date',
-'tbl_transaction.rpt_dt1 as report_date','tbl_transaction.unit_no as unit_number','tbl_transaction.COMPONENT as component_name','tbl_transaction.model','tbl_transaction.oil_change','tbl_transaction.filter_code','(select "") as cmp',
-'(select "") as mp','(select case tbl_transaction.eval_code when "N" then "Normal"
-when "B" then "Attention"
-when "C" then "Urgent"
-when " " then "Normal"
-when null then "Normal" end) as eval_code','(select "")as blank'])
-  ->from('tbl_transaction')
-  ->where(['between','tbl_transaction.rpt_dt1',$date1,$date2]);
-return $query->all();
+  $sql="tbl_transaction.grouploc,tbl_transaction.branch,tbl_transaction.name,tbl_transaction.Lab_No,
+tbl_transaction.SAMPL_DT1,tbl_transaction.RECV_DT1,tbl_transaction.RPT_DT1,
+tbl_transaction.UNIT_NO,tbl_transaction.COMPONENT,tbl_transaction.MODEL,
+tbl_transaction.oil_change,tbl_transaction.EVAL_CODE";
+$where="tbl_transaction.rpt_dt1 > (DATE_SUB(CURDATE(), INTERVAL 3 YEAR)) and (tbl_transaction.RPT_DT1 between '$date1' and '$date2')";
+
+$datatables = (new DataTables\Builder())
+    ->withQueryBuilder(
+        $conn->createQueryBuilder()
+            ->select($sql)
+            ->from('tbl_transaction')
+            ->where($where)
+    )
+    ->withRequestParams($_REQUEST);
+return $datatables->getResponse();
 }
 
 //data fmcpusat
